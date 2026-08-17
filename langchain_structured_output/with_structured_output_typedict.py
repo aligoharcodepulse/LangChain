e5f04043dc89127d17e46f56166310c0e794ec4a
Muhammad Ali Gohar <aligoharkp@gmail.com>
2026-08-17 20:07:56 +0500

    Pratice structured output

--- langchain_structured_output/with_structured_output_typedict.py
+++ langchain_structured_output/with_structured_output_typedict.py
@@ -1,6 +1,6 @@
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 from dotenv import load_dotenv
-from typing import TypedDict, Annotated
+from typing import TypedDict, Annotated, Optional, Literal
 
 load_dotenv()
 
@@ -13,8 +13,12 @@ llm = HuggingFaceEndpoint(
 model = ChatHuggingFace(llm=llm)
 
 class Review(TypedDict):
+    key_themes: Annotated[list[str], "Write down all the key themes discussed in the review in a list"]
     summary: Annotated[str, "A brief summary of the review"] 
-    sentiment: Annotated[str, "Return sentiment of the review either positive, negative or neutral"] 
+    sentiment: Annotated[Literal['pos','neg'], "Return sentiment of the review either positive, negative or neutral"] 
+    pros: Annotated[Optional[list[str]], "Write down all the pros inside a list"]
+    cons: Annotated[Optional[list[str]], "Write down all the cons inside a list"]
+    name: Annotated[Optional[str], "Write the name of the reviewer?"]
 
 structured_model = model.with_structured_output(Review)
 
@@ -30,6 +34,8 @@ Cons:
 Bulky and heavy-not great for one-handed use
 Bloatware still exists in One UI
 Expensive compared to competitors
+
+Reviewed By Ali Gohar
 """)
 
 print(result)
