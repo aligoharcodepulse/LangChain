3ce3117853315985651408f356c93ef64d80b85a
Muhammad Ali Gohar <aligoharkp@gmail.com>
2026-08-22 11:31:36 +0500

    Practice: MultiQuery Retriever

---
+++ langchain_retrievers/multi_query_retriever.py
@@ -0,0 +1,69 @@
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.documents import Document
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_classic.retrievers import MultiQueryRetriever
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation",
+    max_new_tokens=200,
+    temperature=0.7
+)
+
+model = ChatHuggingFace(llm=llm)
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+# Relevant health & wellness documents
+all_docs = [
+    Document(page_content="Regular walking boosts heart health and can reduce symptoms of depression.", metadata={"source": "H1"}),
+    Document(page_content="Consuming leafy greens and fruits helps detox the body and improve longevity.", metadata={"source": "H2"}),
+    Document(page_content="Deep sleep is crucial for cellular repair and emotional regulation.", metadata={"source": "H3"}),
+    Document(page_content="Mindfulness and controlled breathing lower cortisol and improve mental clarity.", metadata={"source": "H4"}),
+    Document(page_content="Drinking sufficient water throughout the day helps maintain metabolism and energy.", metadata={"source": "H5"}),
+    Document(page_content="The solar energy system in modern homes helps balance electricity demand.", metadata={"source": "I1"}),
+    Document(page_content="Python balances readability with power, making it a popular system design language.", metadata={"source": "I2"}),
+    Document(page_content="Photosynthesis enables plants to produce energy by converting sunlight.", metadata={"source": "I3"}),
+    Document(page_content="The 2022 FIFA World Cup was held in Qatar and drew global energy and excitement.", metadata={"source": "I4"}),
+    Document(page_content="Black holes bend spacetime and store immense gravitational energy.", metadata={"source": "I5"}),
+]
+
+vectorstore = FAISS.from_documents(
+    documents=all_docs,
+    embedding=embeddings
+)
+
+# Create Retrievers
+# Simple
+similarity_retriever = vectorstore.as_retriever(
+    search_type = "similarity", 
+    search_kwargs={'k':5}
+)
+
+# Multi-Query
+multiquery_retriever = MultiQueryRetriever.from_llm(
+    retriever = vectorstore.as_retriever(search_kwargs={'k':2}),
+    llm = model
+)
+
+query = "How to improve energy levels and maintain balance?"
+
+similarity_results = similarity_retriever.invoke(query)
+multiquery_results = multiquery_retriever.invoke(query)
+
+for i, doc in enumerate(similarity_results):
+    print(f"\n--- Result {i+1} ---")
+    print(f"Content:\n {doc.page_content}...") 
+
+print('*'*120)
+
+for i, doc in enumerate(multiquery_results):
+    print(f"\n--- Result {i+1} ---")
+    print(f"Content:\n {doc.page_content}...") 
+
