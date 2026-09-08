170882a12da62077e4cee37c2d3b48caaaed22c2
Muhammad Ali Gohar <aligoharkp@gmail.com>
2026-09-08 11:41:15 +0500

    Step 3: Augmentation inside youtube chatbot

--- youtube_chatbot/chatbot.py
+++ youtube_chatbot/chatbot.py
@@ -1,6 +1,6 @@
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, IpBlocked
 from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings, ChatHuggingFace
+from langchain_huggingface import HuggingFaceEmbeddings, ChatHuggingFace, HuggingFaceEndpoint
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
@@ -45,7 +45,38 @@ vectorstore = FAISS.from_documents(chunks, embeddings)
 retriever = vectorstore.as_retriever(search_type='similarity',search_kwargs={"k":4})
 # print(retriever)
 query = retriever.invoke("What is large language model?")
-print(query)
+#print(query)
 
 
+# Step - 3 (Augmentation)
+llm = HuggingFaceEndpoint(
+    repo_id="HuggingFaceH4/zephyr-7b-beta",
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=0.2
+)
+
+model = ChatHuggingFace(llm=llm)
+
+prompt = PromptTemplate(
+    template="""
+    You are a helpful assistant.
+    Answer ONLY from the provided transcript context.
+    If the context is insufficient, just say you don't know.
+
+    {context}
+
+    Question: {question}
+    """,
+    input_variables=["context", "question"]
+)
+
+
+question = "Is the topic of llm discussed in this video? if yes then what was discussed?"
+retrieved_docs = retriever.invoke(question)
+#print(retrieved_docs)
+
+context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
+print(context_text)
+final_prompt = prompt.invoke({"context":context_text, "question":question})
 
