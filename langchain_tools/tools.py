82b9a3b5c48db2ccd7b25d05489c29e3fcf826b6
Muhammad Ali Gohar <aligoharkp@gmail.com>
2026-09-16 21:12:18 +0500

    Created tool with StructuredTool and Pydantic

--- langchain_tools/tools.py
+++ langchain_tools/tools.py
@@ -1,5 +1,7 @@
 from langchain_community.tools import DuckDuckGoSearchRun
-from langchain_core.tools import tool
+from langchain_core.tools import tool, StructuredTool
+from pydantic import BaseModel, Field
+
 
 #Built-in tools
 search_tool = DuckDuckGoSearchRun()
@@ -33,7 +35,38 @@ def multiply(a:int, b:int) -> int:
     return a*b
 
 result = multiply.invoke({"a":3,"b":5})
+# print(result)
+# print(multiply.name)
+# print(multiply.description)
+# print(multiply.args)
+
+
+
+# Using Structured Tool
+class MultiplyInput(BaseModel):
+    a: int = Field(
+        required=True,
+        description="The first number to add"
+    )
+    b: int = Field(
+        required=True,
+        description="The first number to add"
+    )
+
+
+def multiply_func(a: int, b: int) -> int:
+    return a * b
+
+
+multiply_tool = StructuredTool.from_function(
+    func=multiply_func,
+    name="multiply",
+    description="Multiply two numbers",
+    args_schema=MultiplyInput
+)
+
+result = multiply_tool.invoke({"a":3,"b":3})
 print(result)
-print(multiply.name)
-print(multiply.description)
-print(multiply.args)
+print(multiply_tool.name)
+print(multiply_tool.description)
+print(multiply_tool.args)
