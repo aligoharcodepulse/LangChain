5f51676f9855aeb9c6ece40afba0c3d83ffd6ed7
Muhammad Ali Gohar <aligoharkp@gmail.com>
2026-09-21 15:08:32 +0500

    Practice: Tool Calling

--- langchain_tools/tool_calling.py
+++ langchain_tools/tool_calling.py
@@ -12,15 +12,15 @@ def multiply(a:int, b:int)->int:
     """Given two number a and b this tool return their product"""
     return a * b
 
-print(multiply.invoke({'a':3, 'b':4}))
-print(multiply.name)
-print(multiply.description)
-print(multiply.args)
+# print(multiply.invoke({'a':3, 'b':4}))
+# print(multiply.name)
+# print(multiply.description)
+# print(multiply.args)
 
 
 # tool binding
 llm = HuggingFaceEndpoint(
-    repo_id="HuggingFaceH4/zephyr-7b-beta",
+    repo_id="deepseek-ai/DeepSeek-R1-0528",
     task="text-generation",
     max_new_tokens=100,
     temperature=0.7,
@@ -29,4 +29,9 @@ llm = HuggingFaceEndpoint(
 model = ChatHuggingFace(llm=llm)
 
 llm_with_tools = model.bind_tools([multiply])
-print(llm_with_tools)
+# print(llm_with_tools)
+
+
+# tool calling
+# print(llm_with_tools.invoke('Hi! How are you?'))
+print(llm_with_tools.invoke('Can you multiply 3 with 10?'))
